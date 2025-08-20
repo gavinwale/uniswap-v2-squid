b20594923b3e8cdc32f158013787812375277dff
Gavin <wale.gavin@gmail.com>
2025-08-19 19:03:57 -0600

    clean merge stuff

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { TypeormDatabase } from '@subsquid/typeorm-store'
 import { DataHandlerContext } from '@subsquid/evm-processor'
-import { processor } from './processor'
+import { processor } from './config'
 import { processBlocks } from './mappings/processor'
 
 processor.run(new TypeormDatabase(), async (ctx) => {
